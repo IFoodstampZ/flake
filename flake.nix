26f4a1c8e5cd34b635e9336224b95abb3ab2188e
ifoodstampz <fowlera6614@gmail.com>
2025-11-24 03:46:21 -0500

    hi

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    #nixcats = {
+      #url = "github:BirdeeHub/nixCats-nvim";
+      #inputs.nixpkgs.follows = "nixpkgs";
+    #};
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+	./noctalia.nix
+	];
+    };
+  };
+}
